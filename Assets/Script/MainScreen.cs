a23775c26e3fa59da7185cdf75bebd84e3fff71c
vishalfbs <you@example.com>
2016-08-23 19:05:11 +0530

    Change is done for testing only

--- Assets/Script/MainScreen.cs
+++ Assets/Script/MainScreen.cs
@@ -4,6 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class MainScreen : MonoBehaviour {
 
+	void Start(){
+		Debug.Log ("This is main scene");
+	}
+
 	void Update(){
 		if (Input.GetKey (KeyCode.Escape)) {
 			Application.Quit ();
